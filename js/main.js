b332bce48fb6f1be26664e284419d9a126f49cd4
Bhakti1808 <200203048115@silveroakuni.ac.in>
2022-09-28 10:08:38 +0530

    notes

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ import { createNote } from "./create.js";
 var container3 = document.getElementsByClassName("container3")[0];
 var checkIcon = document.getElementById("check-icon");
 var xIcon = document.getElementById("x-icon");
-// var txt = document.getElementById("txt")
 var box = document.getElementById("good");
  
 xIcon.addEventListener("click",function(){
